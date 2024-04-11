7cc8a277bd28c734a7724e4e5649e7a6a37c2f53
Geon.C <chlrjs1324@gmail.com>
2024-04-11 18:07:40 +0900

    ~ 섹션4 전체 내용

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -22,6 +23,8 @@ class CafeKioskTest {
     }
 
     @Test
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     void add() {
         CafeKiosk kiosk = new CafeKiosk();
         kiosk.add(new Americano());
@@ -79,7 +82,9 @@ class CafeKioskTest {
     }
 
     @Test
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     void calculateTotalPrice() {
+        // given
         CafeKiosk kiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -87,8 +92,10 @@ class CafeKioskTest {
         kiosk.add(americano);
         kiosk.add(latte);
 
+        // when
         int totalPrice = kiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
