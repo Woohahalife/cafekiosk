7eea6af2cf1d870d089daf9513a47d3444389880
Geon.C <chlrjs1324@gmail.com>
2024-04-25 14:08:42 +0900

    ~ 섹션7 - @ParameterizedTest

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,12 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,5 +41,37 @@ class ProductTypeTest {
         // then
         assertThat(result1).isTrue();
         assertThat(result2).isTrue();
+    }
+
+    // @ParameterizedTest + @CsvSource
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @ParameterizedTest(name = "{index} => {0}일 경우 {1} 이어야 한다.")
+    @CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
+    void containsStockTypeForCsvSource(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    // @ParameterizedTest + @MethodSource
+    @ParameterizedTest
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @MethodSource("provideProductTypesForCheckingStockType")
+    void containsStockTypeForMethodSource(ProductType productType, boolean expected) {
+        // when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
     }
 }
